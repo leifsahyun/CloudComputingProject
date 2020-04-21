5bf1c1730588af9ebb09198facd0f171991f328b
Ozan Akyildiz <oakyildiz@wpi.edu>
2020-04-21 12:17:49 -0400

    recommender with linear reward fcn

--- recommender.py
+++ recommender.py
@@ -21,8 +21,9 @@ class Recommender(object):
     # the reccommender has 2 timers one for the regular metrics data update
     # the second, optional one is to check if the current instance is able to satisfy requirements 
     def __init__(self, min_interval):
+        self._update_t=None
         #self._update_t=threading.Timer(min_interval*60, hello)
-        self.self_t=None
+        self._self_t=None
         self.metrics = {}
         self.parms = {}
 
@@ -32,7 +33,7 @@ class Recommender(object):
     #update timer methods
 
     def init_self_check(self, sec_interval):
-        raise NotImplemented
+        raise NotImplementedError
 
     # def start(self):
         # self._update_t(start)
@@ -42,7 +43,7 @@ class Recommender(object):
     #consider making this a startic_method
     def get_candidates():    
     # look up 
-        raise NotImplemented
+        raise NotImplementedError
 
     def eval_score(self):
         pass
@@ -61,13 +62,16 @@ class Recommender(object):
     def self_check(self):
         pass
 
-    def recommend(self):
-        return "provider, size"
+    def recommend(self,instances):
+          
+        scores = [key:self.eval(instance) for key, instance param in instances]
 
-    def __del__(self): 
-        print("Terminating Recommender...")
-        self._update_t.cancel()
-        self._self_t.cancel()
+        best=min(dic, key=dic.get)
+        print(" The instance " + instances[best]+" was evaluated as the best candidate")
+
+        return instances[best] 
+
+        #make this more analytic, with non-certain scores and close alternatives
 
 
     def eval(self,instance):
@@ -76,6 +80,14 @@ class Recommender(object):
         for key, param in self.metrics:
            sum+=instance[key].eval(stats)
         return sum
+
+    def __del__(self): 
+        print("Terminating Recommender...")
+        self._update_t.cancel()
+        self._self_t.cancel()
+
+
+
 
 
 
@@ -100,10 +112,10 @@ class Metric(object):
         self.name = name
         self.treshold = t
         self.alpha = alpha
-    if isinstance(t,list):
-        self.set_type(TYPE.BAND)
-    else:
-        self.set_type(TYPE.type_id)
+        if isinstance(t, list):
+            self.set_type(Metric.TYPE.BAND)
+        else:
+            self.set_type(type_id)
 
     # this wrapper takes a Metric.TYPE or int
     # implemented for future work
@@ -111,19 +123,32 @@ class Metric(object):
         self._type=Metric.TYPE(type_id)
         
         if self._type == "more": #TODO: enumerate this
-            self.error= lambda v: v-self.t
+            self.error= lambda v: v-self.treshold
+            self.op=lambda v: v>self.treshold
+
         elif self._type == "less":
-            self.error= lambda v: self.t-v
+            self.error= lambda v: self.treshold-v
+            self.op=lambda v: v<self.treshold
+
         elif self._type == "band" :
-            self.error= lambda v: v-sum(self.t)/2
+            self.error= lambda v: v-sum(self.treshold)/2
+            self.op=lambda v: self.treshold[0]<v<self.treshold[1]
+
         elif self._type == "bool":
-            self.error= lambda v: int(v == self.t)
+            self.error= lambda v: int(v == self.treshold)
+            self.op= lambda v: int(v == self.treshold)
+
         else:   
+            print "Param type is invalid"
             pass #-1
 
     def eval(self, val):
-        return self.error(val)*alpha
-    
+        return self.error(val)*self.alpha
+
+    #just a wrapper to have constant access to op
+    #TODO: hide op and error from the user
+    def eval_bool(self, val):
+        return self.op(val)
         
 
 
